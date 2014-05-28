5c478ea0b18567593bc12dcb1d2b02187844a9ef
davidhuff2014 <David.Huff@computer-critters.com>
2014-05-27 22:43:33 -0400

    started javascript changes

---
+++ public/application.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(document).on('click', '#hit input', function() {
+    $.ajax({
+      type: 'POST',
+      url: '/game/player/hit'
+    }).done(function(msg) {
+      $('#game').replaceWith(msg);
+    });
+    return false;
+  });
+  
+});
